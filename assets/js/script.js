16d96bc6f67748080c2b9e5b964bfe00487a32b8
Azambik <71612017+Azambik@users.noreply.github.com>
2020-12-08 20:20:51 -0500

    small edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -158,11 +158,11 @@ function movieClickHandler(event) {
 
 function addToWatchList() {
     //working on duplicate add to watch list
-    /*var searched = movieWatchList.indexOf(omdbDataObject); //check if movie has already been searched for and return index
+    var searched = movieWatchList.indexOf(omdbDataObject); //check if movie has already been searched for and return index
     console.log(searched);
     if (searched>-1) { //if movie was found remove from array
         console.log("test");
-    }*/
+    }
     movieWatchList.push(omdbDataObject);
     localStorage.setItem("watchList", JSON.stringify(movieWatchList));
 }
